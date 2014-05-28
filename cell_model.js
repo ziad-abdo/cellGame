f9cb58411b10b168f187c3ef263a79f65db7151d
Ziad Abdo <apprentice@dbc06.local>
2014-05-27 21:34:05 -0700

    created instance method/variable to fill cell with 100 cells

--- cell_model.js
+++ cell_model.js
@@ -1,9 +1,15 @@
 function CellModel(){
-  this.allPossibleCells = []
+  this.allPossibleCells = this.fillArray()
 }
 
 CellModel.prototype= {
-
+  fillArray: function(){
+    array = new Array()
+    for(var i=0; i<100; i++){
+      array.push(new Cell)
+    }
+    return array
+  }
 }
 
 function Cell(){
