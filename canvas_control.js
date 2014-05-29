4db03ec9fdd63d15d2e7a347d08e0b85a11c3c6b
Ziad Abdo <mr.ziad.abdo@gmail.com>
2014-05-29 11:41:00 -0700

    Title: Generating more cell logic   *I added a next state value for cell objects. I was having a problem     trying to figure out how to solve the problem of checking the     surrounding of each cell. If I changed the state of each cell as     I checked them, then I would theoretically end up with many more     dead cells because some cells would be dying before the next cell     would get to count it as a live neighbor. By saving their next     state behavior, each cell will have a chance to view the other     as a neighbor regardless of its next state. Once all the cells     have been checked I will change their current state to be what     their next state is, and then refresh the page. hopefully this     will result in an accurate simulation.   *Please help refactor. T_T

--- canvas_control.js
+++ canvas_control.js
@@ -1,6 +1,7 @@
 window.onload = function(){
   mainControl = new LifeController()
   mainControl.setup()
+  mainControl.fourSquareStart()
 }
 
 
@@ -15,8 +16,23 @@ LifeController.prototype = {
   },
 
   cellScan: function(){
+    for (var y = 0; y<=(this.Model.arrayLength-1); y++){
+      this.Model.allPossibleCells[i].checkNeighbors()
+    }
+  },
+
+  imageScan: function(){
     for (i = 0; i<=(this.Model.arrayLength-1); i++){
       this.View.setDisplay(this.Model.allPossibleCells[i].alive, this.Model.allPossibleCells[i].position)
     }
+  },
+
+  fourSquareStart: function(){
+    this.Model.allPossibleCells[36].alive = true
+    this.Model.allPossibleCells[37].alive = true
+    this.Model.allPossibleCells[46].alive = true
+    this.Model.allPossibleCells[47].alive = true
+    this.Model.allPossibleCells[44].alive = true
+    // this.Model.allPossibleCells[44].nextState = true
   }
 }
--- cell_model.js
+++ cell_model.js
@@ -15,9 +15,31 @@ CellModel.prototype= {
 
 function Cell(indexP){
   this.alive = false
+  this.nextState = false
   this.position = indexP
 }
 
 Cell.prototype={
+  checkNeighbors: function(){
+    environmentArray = mainControl.Model.allPossibleCells
+    allChecksArray = [1, -1, 10, -10, -11, -9, 11, 9]
+    neighborsState = []
+// debugger
+    for(var x=0; x<8; x++){
+      if(environmentArray[(this.position+allChecksArray[x])].alive){
+        neighborsState.push(1)
+      }
+    }
+    this.nextState = this.cellBehavior(neighborsState.length)
+  },
+  cellBehavior: function(value){
+    if (value<2){
+      return false
+    }
+    if (value>4){
+      return false
+    }
+    return true
+  }
 
 }
