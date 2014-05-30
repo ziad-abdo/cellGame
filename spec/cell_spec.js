30f92dd0a9d6a446313ab026a43992bc584a9d96
Ziad Abdo <mr.ziad.abdo@gmail.com>
2014-05-29 21:50:02 -0700

    Initialize suite for cell model testing   * Cell model array gets filled   * Cell model array has cell objects   * Remove hello world test

--- spec/cell_spec.js
+++ spec/cell_spec.js
@@ -1,5 +1,14 @@
-describe( "Hello cell", function() {
-    it( "says cell", function() {
-        expect( helloCell() ).toEqual( "helloCell" );
-    });
-});
+describe("Cell Testing: ", function(){
+  beforeEach( function(){
+    testCellModel = new CellModel()
+    testCell = new Cell()
+  })
+
+  it( "fills an array with cell objects", function(){
+    expect( testCellModel.fillArray().length).toEqual( 100 )
+  })
+
+  it("has generated an array of cells", function(){
+    expect( testCellModel.allPossibleCells[50].alive ).toEqual( testCell.alive )
+  })
+})
--- src/test.js
+++
@@ -1,3 +0,0 @@
-function helloCell(){
-  return "helloCell"
-}
