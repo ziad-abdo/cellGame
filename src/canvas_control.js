fed69751eb5a8dcc4a2544a7698be71b8cd7ca2b
Ziad Abdo <mr.ziad.abdo@gmail.com>
2014-05-30 01:05:50 -0700

    made everything baller as fuck. ask me about it   *changed classes to id's for easier js selecting   *took away previous hacked starting method   *added listeners for each pattern's button   *manipulated starting functions to include time intervals     *starting functions also reset the time interval

--- src/canvas_control.js
+++ src/canvas_control.js
@@ -1,17 +1,30 @@
 window.addEventListener( 'load', function(){
   mainControl = new LifeController()
-  mainControl.gliderCellStart()
+  mainControl.bindEventListeners()
   mainControl.setup()
-  window.setInterval(mainControl.runSet.bind(mainControl), 700)
+
+  mainControl.gliderCellStart()
+
 })
 
 
 function LifeController(){
   this.View = new CanvasView();
   this.Model = new CellModel()
+  this.interval = setInterval()
 }
 
 LifeController.prototype = {
+  bindEventListeners: function(){
+    var glider = document.getElementById("glider")
+    var tetris = document.getElementById("tetris")
+    var fourSquare = document.getElementById("four-square")
+
+    glider.addEventListener('click', this.gliderCellStart.bind(this), false)
+    tetris.addEventListener('click', this.tetrisCellStart.bind(this), false)
+    fourSquare.addEventListener('click', this.fourSquareCellStart.bind(this), false)
+  },
+
   setup: function(){
     this.View.setScreen()
     this.imageScan()
@@ -40,25 +53,31 @@ LifeController.prototype = {
     }
   },
 
-  fourSquareStart: function(){
+  fourSquareCellStart: function(){
+    clearInterval( this.interval )
     this.Model.allPossibleCells[36].alive = true
     this.Model.allPossibleCells[37].alive = true
     this.Model.allPossibleCells[46].alive = true
     this.Model.allPossibleCells[47].alive = true
     this.Model.allPossibleCells[44].alive = true
+    this.interval = setInterval(this.runSet.bind(this), 70)
   },
   tetrisCellStart: function(){
-  //works if it isnt touching edges,.... need to configure edge logic.
+    //works if it isnt touching edges,.... need to configure edge logic.
+    clearInterval( this.interval )
     this.Model.allPossibleCells[35].alive = true
     this.Model.allPossibleCells[44].alive = true
     this.Model.allPossibleCells[45].alive = true
     this.Model.allPossibleCells[46].alive = true
+    this.interval = setInterval(this.runSet.bind(this), 70)
   },
   gliderCellStart: function(){//ITS NOT FUCKED. GLIDER FUCKING WORKS
+    clearInterval( this.interval )
     this.Model.allPossibleCells[32].alive = true
     this.Model.allPossibleCells[33].alive = true
     this.Model.allPossibleCells[34].alive = true
     this.Model.allPossibleCells[24].alive = true
     this.Model.allPossibleCells[13].alive = true
+    this.interval = setInterval(this.runSet.bind(this), 70)
   }
 }
