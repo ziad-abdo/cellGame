5ed47caae1a8c2644f105d71870693271a99a124
Ziad <mr.ziad.abdo@gmail.com>
2018-10-28 14:53:18 -0700

    added a clear button to index.html. This allows the user to reset the canvas if they want to. Some of the presets for cells that get put on the page end up infinitely staying in one position. This allows the user to reset and begin the emulation anew.

--- spec/life_controller_spec.js
+++ spec/life_controller_spec.js
@@ -1,6 +1,17 @@
 describe("LifeController: ", function() {
   it("will be initialized without problems", function() {
     var subject = new LifeController();
-    expect(subject.view).toBeUndefined();
+    expect(subject.Interval).toBeUndefined();
+  })
+
+  describe("resetEmulation", function(){
+    it("will clear interval, and reset the view and model", function() {
+      var subject = new LifeController();
+      var oldView = subject.View
+
+      subject.resetEmulation();
+      expect(oldView == subject.View).toBeFalsy();
+      expect(subject.Interval).toBeNull();
+    })
   })
 });
--- src/canvas_control.js
+++ src/canvas_control.js
@@ -2,15 +2,12 @@ window.addEventListener( 'load', function(){
   mainControl = new LifeController()
   mainControl.bindEventListeners()
   mainControl.setup()
-
-  mainControl.gliderCellStart()
-
 })
 
 
 function LifeController(){
   this.View = new CanvasView();
-  this.Model = new CellModel()
+  this.Model = new CellModel();
 }
 
 LifeController.prototype = {
@@ -18,12 +15,13 @@ LifeController.prototype = {
     var glider = document.getElementById("glider")
     var tetris = document.getElementById("tetris")
     var fourSquare = document.getElementById("four-square")
+    var clearButton = document.getElementById("game-clear")
 
     glider.addEventListener('click', this.gliderCellStart.bind(this), false)
     tetris.addEventListener('click', this.tetrisCellStart.bind(this), false)
     fourSquare.addEventListener('click', this.fourSquareCellStart.bind(this), false)
+    clearButton.addEventListener('click', this.resetEmulation.bind(this), false)
   },
-
   setup: function(){
     this.View.setScreen()
     this.imageScan()
@@ -33,7 +31,11 @@ LifeController.prototype = {
       this.cellNextStep()
       this.imageScan()
   },
-
+  resetEmulation: function(){
+    this.View = new CanvasView();
+    this.Model = new CellModel;
+    this.Interval = null;
+  },
   cellScan: function(){
     for (var y = 0; y<=(this.Model.arrayLength-1); y++){
       this.Model.allPossibleCells[y].checkNeighbors()
@@ -44,38 +46,36 @@ LifeController.prototype = {
       this.Model.allPossibleCells[q].Step()
     }
   },
-
   imageScan: function(){
     for (i = 0; i<=(this.Model.arrayLength-1); i++){
       this.View.setDisplay(this.Model.allPossibleCells[i].alive, this.Model.allPossibleCells[i].position)
     }
   },
-
   fourSquareCellStart: function(){
-    clearInterval( this.interval )
+    clearInterval( this.Interval )
     this.Model.allPossibleCells[36].alive = true
     this.Model.allPossibleCells[37].alive = true
     this.Model.allPossibleCells[46].alive = true
     this.Model.allPossibleCells[47].alive = true
     this.Model.allPossibleCells[44].alive = true
-    this.interval = setInterval(this.runSet.bind(this), 600)
+    this.Interval = setInterval(this.runSet.bind(this), 600)
   },
   tetrisCellStart: function(){
     //works if it isnt touching edges,.... need to configure edge logic.
-    clearInterval( this.interval )
+    clearInterval( this.Interval )
     this.Model.allPossibleCells[35].alive = true
     this.Model.allPossibleCells[44].alive = true
     this.Model.allPossibleCells[45].alive = true
     this.Model.allPossibleCells[46].alive = true
-    this.interval = setInterval(this.runSet.bind(this), 600)
+    this.Interval = setInterval(this.runSet.bind(this), 600)
   },
-  gliderCellStart: function(){//ITS NOT FUCKED. GLIDER FUCKING WORKS
-    clearInterval( this.interval )
+  gliderCellStart: function(){
+    clearInterval( this.Interval )
     this.Model.allPossibleCells[32].alive = true
     this.Model.allPossibleCells[33].alive = true
     this.Model.allPossibleCells[34].alive = true
     this.Model.allPossibleCells[24].alive = true
     this.Model.allPossibleCells[13].alive = true
-    this.interval = setInterval(this.runSet.bind(this), 600)
+    this.Interval = setInterval(this.runSet.bind(this), 600)
   }
 }
